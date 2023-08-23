6b6fa1fca5ac3cfb03ee2412c18f3ddda35dc307
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-08-22 22:04:20 -0400

    instlar bootstrap y sass

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+
+    <h1>Hola mundo</h1>
+    <button class="btn btn-primary">Holaaa</button>
   </div>
 </template>
 
