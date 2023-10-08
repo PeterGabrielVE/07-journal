7c48f4094dadd048dd6b1f656450e79552b50e1d
Pedro Gabriel <31576642+PeterGabrielVE@users.noreply.github.com>
2023-10-07 20:52:08 -0400

    probar mapeo

--- tests/unit/modules/daybook/router/index.spec.js
+++ tests/unit/modules/daybook/router/index.spec.js
@@ -32,5 +32,20 @@ describe('Pruebas en el router module del Daybook', () =>{
 
         expect( routes ).toContain('EntryView')
         expect( routes ).toContain('NoEntrySelected')
+
     })
+
+    test('debe de retornar el id de la ruta', () => {
+        
+        const route = {
+            params: {
+                id: 'ABC-123'
+            }
+        }
+
+        // expect( daybookRouter.children[1].props( route ) ).toEqual({ id: 'ABC-123' })
+        const entryRoute = daybookRouter.children.find( route => route.name === 'entry' )
+        expect( entryRoute.props( route ) ).toEqual({ id: 'ABC-123' })
+    })
+    
 })
